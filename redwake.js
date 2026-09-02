42ac4fbe00b40d0b9e357e93267648fb2dd39801
Happy Starfish <19838555+rahulpanicker@users.noreply.github.com>
2026-09-01 18:38:20 -0700

    Add Redwake XHR loader (redwake.js)

---
+++ redwake.js
@@ -0,0 +1,23 @@
+(function(){
+  var files=["redwake-a1.js?v=c8f41e","redwake-a2.js?v=c8f41e","redwake-a3.js?v=c8f41e","redwake-a4.js?v=c8f41e"];
+  var acc="", i=0;
+  function step(){
+    if(i>=files.length){
+      var s=document.createElement("script");
+      s.text=acc;
+      document.body.appendChild(s);
+      return;
+    }
+    var x=new XMLHttpRequest();
+    x.open("GET", files[i++], true);
+    x.onreadystatechange=function(){
+      if(x.readyState===4){
+        if(x.status>=200 && x.status<300) acc+=x.responseText;
+        else console.error("Redwake load fail", files[i-1], x.status);
+        step();
+      }
+    };
+    x.send();
+  }
+  step();
+})();
